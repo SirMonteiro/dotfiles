6e48e4b83c18b9d48720743dd0561999838383ad
Gabriel de Souza <gabrielmonteirodesouza@tutanota.com>
2025-09-05 18:09:59 -0300

    fix: remove package installation using dotfiles

--- .chezmoiscripts/run_onchange_install-theme.sh
+++ .chezmoiscripts/run_onchange_install-theme.sh
@@ -2,7 +2,7 @@
 
 git clone --depth 1 https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme.git /tmp/catppuccin-gtk
 cd /tmp/catppuccin-gtk/themes
-./install.sh -l -s compact -t purple teal
+./install.sh -l -s compact -t mauve teal
 flatpak override --user --filesystem=$HOME/.themes
 flatpak override --user --filesystem=xdg-config/gtk-4.0
 
@@ -16,8 +16,9 @@ curl -LO https://github.com/catppuccin/kde/releases/latest/download/Mocha-color-
 tar xf Mocha-color-schemes.tar.gz && rm -f Mocha-color-schemes.tar.gz
 cp Mocha-color-schemes/* $HOME/.local/share/color-schemes
 
-paru -S papirus-folders-catppuccin-git
+#paru -S --needed papirus-folders-catppuccin-git
 papirus-folders -C cat-mocha-teal --theme Papirus-Dark
 
 git clone --depth 1 https://github.com/catppuccin/konsole.git /tmp/catppuccin-konsole
 cp /tmp/catppuccin-konsole/themes/* $HOME/.local/share/konsole
+
